7abcb161353f89e3d5b9471b1309b8af3f8f060a
Arian <arian.koochak@protonmail.com>
2024-10-03 14:37:26 +0330

    add 3 tests for manualSort

---
+++ source/tests/manualSort.test.js
@@ -0,0 +1,29 @@
+const { manualSort } = require("..");
+const { StudentList, SortedStudentList, UsersDatas, SortedUsersDatas, nestedArray, SortedNestedArray } = require("./arraysForTests");
+
+test("manualSort => StudentList test", () => {
+    const test = () => {
+        return manualSort(StudentList,{keyOfObject : 'score',arrayCondition : 'random'})
+    };
+    expect(test()).toEqual(SortedStudentList);
+});
+
+test("manualSort => UsersDatas test", () => {
+    const test = () => {
+        return manualSort(UsersDatas, {
+            keyOfObject: "purchaseCount",
+            arrayCondition: "random",
+        });
+    };
+    expect(test()).toEqual(SortedUsersDatas);
+});
+
+test("manualSort => nestedArray test", () => {
+    const test = () => {
+        return manualSort(nestedArray, {
+            nestedArrayIndex : '2.0',
+            arrayCondition: "random",
+        });
+    };
+    expect(test()).toEqual(SortedNestedArray);
+});
