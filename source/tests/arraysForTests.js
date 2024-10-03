8e681e54af3337f95268c5f4cb10ec33778a6c51
Arian <arian.koochak@protonmail.com>
2024-10-03 10:36:54 +0330

    add one array for test

---
+++ source/tests/arraysForTests.js
@@ -0,0 +1,35 @@
+const StudentList = [
+    {
+        name: "Abbas",
+        lastName: "Elahi",
+        score: "19",
+    },
+    {
+        name: "Arian",
+        lastName: "Elahi",
+        score: "18",
+    },
+    {
+        name: "Hossein",
+        lastName: "Elahi",
+        score: "19",
+    },
+    ,
+    {
+        name: "Ali",
+        lastName: "mohammadi",
+        score: "15.5",
+    },
+    ,
+    {
+        name: "Reza",
+        lastName: "Hosseini",
+        score: "11",
+    },
+    ,
+    {
+        name: "Jafar",
+        lastName: "Karimi",
+        score: "14",
+    },
+];
