ce70c42bb5a2124105211538adf5cf4b3aeae2b0
Arian <arian.koochak@protonmail.com>
2024-10-04 22:59:26 +0330

    add bubble sort algorithm

--- source/utils/sortFuncs.js
+++ source/utils/sortFuncs.js
@@ -6,7 +6,16 @@ const sorts = {
         console.log(arr);
     },
     bubble: function (arr) {
-        console.log(arr);
+        for (let i = 0; i < arr.length; i++) {
+            for (let j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j][1] > arr[j + 1][1]) {
+                    let temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
     },
     merge: function (arr) {
         console.log(arr);
