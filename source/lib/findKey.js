d1a07c1eb3379b9658bcc6fcf3668084ed97fc8c
Arian <arian.koochak@protonmail.com>
2024-10-05 09:19:33 +0330

    fix findKey bug

--- source/lib/findKey.js
+++ source/lib/findKey.js
@@ -1,8 +1,10 @@
-module.exports = function (obj, target){
+function findKey(obj, target){
     return target in obj
         ? obj[target]
         : Object.values(obj).reduce((acc, val) => {
               if (acc !== undefined) return acc;
-              if (typeof val === "object") return findKeys(val, target);
+              if (typeof val === "object") return findKey(val, target);
           }, undefined);
 };
+
+module.exports = findKey
