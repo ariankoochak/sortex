72594c32a8e0a1946a4be40a5162916c375ff95f
Arian <arian.koochak@protonmail.com>
2024-10-06 11:48:01 +0330

    add insertionSort

---
+++ source/tests/manualSort.insertion.test.js
@@ -0,0 +1,51 @@
+const { manualSort } = require("..");
+const {
+    StudentList,
+    SortedStudentList,
+    UsersDatas,
+    SortedUsersDatas,
+    nestedArray,
+    SortedNestedArray,
+    UsersDatasArrayMode,
+    SortedUsersDatasArrayMode,
+} = require("./arraysForTests");
+
+test("manualSort => insertionSort test with arrayCondition", () => {
+    const test = () => {
+        return manualSort(StudentList, {
+            valuePath: "score",
+            sortAlgorithm: "insertion",
+        });
+    };
+    expect(test()).toEqual(SortedStudentList);
+});
+
+test("manualSort => insertionSort test with arrayCondition", () => {
+    const test = () => {
+        return manualSort(UsersDatas, {
+            valuePath: "purchase.purchaseCount",
+            sortAlgorithm: "insertion",
+        });
+    };
+    expect(test()).toEqual(SortedUsersDatas);
+});
+
+test("manualSort => insertionSort test with arrayCondition", () => {
+    const test = () => {
+        return manualSort(UsersDatasArrayMode, {
+            valuePath: "purchase.0",
+            sortAlgorithm: "insertion",
+        });
+    };
+    expect(test()).toEqual(SortedUsersDatasArrayMode);
+});
+
+test("manualSort => insertionSort test with arrayCondition", () => {
+    const test = () => {
+        return manualSort(nestedArray, {
+            valuePath: "2.0",
+            sortAlgorithm: "insertion",
+        });
+    };
+    expect(test()).toEqual(SortedNestedArray);
+});
--- source/utils/sortFuncs.js
+++ source/utils/sortFuncs.js
@@ -1,6 +1,16 @@
 const sorts = {
     insertion: function (arr) {
-        console.log(arr);
+        for (let i = 1; i < arr.length; i++) {
+            let key = arr[i];
+            let j = i - 1;
+
+            while (j >= 0 && arr[j][1] > key[1]) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+        return arr;
     },
     selection: function (arr) {
         console.log(arr);
