75a5a7e4ae28cbb2c69936e94743723b22165676
Arian <arian.koochak@protonmail.com>
2024-10-06 14:28:02 +0330

    add test for nearly sorted array

--- source/tests/automateSort/automateSort.test.js
+++ source/tests/automateSort/automateSort.test.js
@@ -6,30 +6,60 @@ const {
     UsersDatasArrayMode,
 } = require("../arraysForTest/randomArray");
 
-test("automateSort", () => {
+const nearlySortedArr = require("../arraysForTest/nearlySortedArray");
+
+test("analyzeArrayCondition => random", () => {
     const test = () => {
         return analyzeArrayCondition(StudentList);
     };
     expect(test()).toEqual('random');
 });
 
-test("automateSort", () => {
+test("analyzeArrayCondition => random", () => {
     const test = () => {
         return analyzeArrayCondition(UsersDatas);
     };
     expect(test()).toEqual("random");
 });
 
-test("automateSort", () => {
+test("analyzeArrayCondition => random", () => {
     const test = () => {
         return analyzeArrayCondition(nestedArray);
     };
     expect(test()).toEqual("random");
 });
 
-test("automateSort", () => {
+test("analyzeArrayCondition => random", () => {
     const test = () => {
         return analyzeArrayCondition(UsersDatasArrayMode);
     };
     expect(test()).toEqual("random");
+});
+
+test("analyzeArrayCondition => nearly sorted", () => {
+    const test = () => {
+        return analyzeArrayCondition(nearlySortedArr.SortedStudentList);
+    };
+    expect(test()).toEqual("nearly-sorted");
+});
+
+test("analyzeArrayCondition => nearly sorted", () => {
+    const test = () => {
+        return analyzeArrayCondition(nearlySortedArr.UsersDatas);
+    };
+    expect(test()).toEqual("nearly-sorted");
+});
+
+test("analyzeArrayCondition => nearly sorted", () => {
+    const test = () => {
+        return analyzeArrayCondition(nearlySortedArr.UsersDatasArrayMode);
+    };
+    expect(test()).toEqual("nearly-sorted");
+});
+
+test("analyzeArrayCondition => nearly sorted", () => {
+    const test = () => {
+        return analyzeArrayCondition(nearlySortedArr.nestedArray);
+    };
+    expect(test()).toEqual("nearly-sorted");
 });
