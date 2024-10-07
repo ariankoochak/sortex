5104d5f8f8bbcc4feb4f523bb40cb838f990fdd4
Arian <arian.koochak@protonmail.com>
2024-10-07 16:14:19 +0330

    update test arrays

--- source/tests/arraysForTest/randomFewUniqueArray.js
+++ source/tests/arraysForTest/randomFewUniqueArray.js
@@ -52,36 +52,16 @@ const StudentList = [
 ];
 
 const SortedStudentList = [
-    {
-        name: "Reza",
-        lastName: "Hosseini",
-        score: "11",
-    },
-    {
-        name: "Jafar",
-        lastName: "Karimi",
-        score: "15.5",
-    },
-    {
-        name: "Ali",
-        lastName: "mohammadi",
-        score: "15.5",
-    },
-    {
-        name: "Arian",
-        lastName: "Elahi",
-        score: "20",
-    },
-    {
-        name: "Hossein",
-        lastName: "Elahi",
-        score: "20",
-    },
-    {
-        name: "Abbas",
-        lastName: "Elahi",
-        score: "20",
-    },
+    { name: "Reza", lastName: "Hosseini", score: "11" },
+    { name: "Ali", lastName: "mohammadi", score: "15.5" },
+    { name: "Jafar", lastName: "Karimi", score: "15.5" },
+    { name: "Arian", lastName: "Elahi", score: "20" },
+    { name: "Arian", lastName: "Elahi", score: "20" },
+    { name: "Arian", lastName: "Elahi", score: "20" },
+    { name: "Hossein", lastName: "Elahi", score: "20" },
+    { name: "Abbas", lastName: "Elahi", score: "20" },
+    { name: "Arian", lastName: "Elahi", score: "20" },
+    { name: "Arian", lastName: "Elahi", score: "20" },
 ];
 
 const UsersDatas = [
@@ -169,52 +149,54 @@ const UsersDatas = [
 
 const SortedUsersDatas = [
     {
-        name: "Reza",
-        lastName: "Hosseini",
-        purchase: {
-            purchaseCount: 0,
-            purchaseSum: "0",
-        },
-    },
-    {
         name: "Ali",
         lastName: "mohammadi",
-        purchase: {
-            purchaseCount: 0,
-            purchaseSum: "21000000",
-        },
+        purchase: { purchaseCount: 0, purchaseSum: "21000000" },
     },
     {
-        name: "Hossein",
-        lastName: "Elahi",
-        purchase: {
-            purchaseCount: 3,
-            purchaseSum: "13000000",
-        },
+        name: "Reza",
+        lastName: "Hosseini",
+        purchase: { purchaseCount: 0, purchaseSum: "0" },
     },
     {
         name: "Abbas",
         lastName: "Elahi",
-        purchase: {
-            purchaseCount: 3,
-            purchaseSum: "23000000",
-        },
+        purchase: { purchaseCount: 3, purchaseSum: "23000000" },
+    },
+    {
+        name: "Hossein",
+        lastName: "Elahi",
+        purchase: { purchaseCount: 3, purchaseSum: "13000000" },
+    },
+    {
+        name: "Hossein",
+        lastName: "Elahi",
+        purchase: { purchaseCount: 3, purchaseSum: "13000000" },
+    },
+    {
+        name: "Hossein",
+        lastName: "Elahi",
+        purchase: { purchaseCount: 3, purchaseSum: "13000000" },
+    },
+    {
+        name: "Hossein",
+        lastName: "Elahi",
+        purchase: { purchaseCount: 3, purchaseSum: "13000000" },
+    },
+    {
+        name: "Hossein",
+        lastName: "Elahi",
+        purchase: { purchaseCount: 3, purchaseSum: "13000000" },
     },
     {
         name: "Arian",
         lastName: "Elahi",
-        purchase: {
-            purchaseCount: 3,
-            purchaseSum: "50000000",
-        },
+        purchase: { purchaseCount: 3, purchaseSum: "50000000" },
     },
     {
         name: "Jafar",
         lastName: "Karimi",
-        purchase: {
-            purchaseCount: 8,
-            purchaseSum: "53000000",
-        },
+        purchase: { purchaseCount: 8, purchaseSum: "53000000" },
     },
 ];
 
@@ -273,19 +255,19 @@ const UsersDatasArrayMode = [
 
 const SortedUsersDatasArrayMode = [
     {
-        name: "Reza",
-        lastName: "Hosseini",
-        purchase: [0, "0"],
-    },
-    {
         name: "Ali",
         lastName: "mohammadi",
         purchase: [0, "21000000"],
     },
     {
-        name: "Hossein",
+        name: "Reza",
+        lastName: "Hosseini",
+        purchase: [0, "0"],
+    },
+    {
+        name: "Arian",
         lastName: "Elahi",
-        purchase: [3, "13000000"],
+        purchase: [3, "50000000"],
     },
     {
         name: "Abbas",
@@ -293,9 +275,9 @@ const SortedUsersDatasArrayMode = [
         purchase: [3, "23000000"],
     },
     {
-        name: "Arian",
+        name: "Hossein",
         lastName: "Elahi",
-        purchase: [3, "50000000"],
+        purchase: [3, "13000000"],
     },
     {
         name: "Jafar",
@@ -338,16 +320,16 @@ const nestedArray = [
 ];
 
 const SortedNestedArray = [
+    ["mohammad", "Karimi", ["11"]],
     ["Reza", "Hosseini", ["11"]],
     ["Jafar", "Karimi", ["11"]],
-    ["mohammad", "Karimi", ["11"]],
     ["Ali", "mohammadi", ["15.5"]],
     ["arian", "Elahi", ["20"]],
     ["Hossein", "Elahi", ["20"]],
-    ["Abbas", "Elahi", ["20"]],
+    ["alireza", "Elahi", ["20"]],
     ["reza", "Elahi", ["20"]],
     ["hamidreza", "Elahi", ["20"]],
-    ["alireza", "Elahi", ["20"]],
+    ["Abbas", "Elahi", ["20"]],
 ];
 
 module.exports = {
