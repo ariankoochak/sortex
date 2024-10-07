17df5160dd7adfd766a263af737c8da6bf9aaffb
Arian <arian.koochak@protonmail.com>
2024-10-07 16:14:34 +0330

    add test for all array type in automate test

---
+++ source/tests/automateSort/automateSort.reversedArray.test.js
@@ -0,0 +1,47 @@
+const { automateSort } = require("../..");
+const {
+    StudentList,
+    SortedStudentList,
+    UsersDatas,
+    SortedUsersDatas,
+    nestedArray,
+    SortedNestedArray,
+    UsersDatasArrayMode,
+    SortedUsersDatasArrayMode,
+} = require("../arraysForTest/reversedArray");
+
+test("automateSort", () => {
+    const test = () => {
+        return automateSort(StudentList, {
+            valuePath: "score",
+        });
+    };
+    expect(test()).toEqual(SortedStudentList);
+});
+
+test("automateSort", () => {
+    const test = () => {
+        return automateSort(UsersDatas, {
+            valuePath: "purchase.purchaseCount",
+        });
+    };
+    expect(test()).toEqual(SortedUsersDatas);
+});
+
+test("automateSort", () => {
+    const test = () => {
+        return automateSort(UsersDatasArrayMode, {
+            valuePath: "purchase.0",
+        });
+    };
+    expect(test()).toEqual(SortedUsersDatasArrayMode);
+});
+
+test("automateSort", () => {
+    const test = () => {
+        return automateSort(nestedArray, {
+            valuePath: "2.0",
+        });
+    };
+    expect(test()).toEqual(SortedNestedArray);
+});
